78107bee0b2fd14558fc051ca970665a88fab9a6
Duncan Mac-Vicar P <dmacvicar@suse.de>
2018-07-18 18:04:06 +0200

    more renames

--- salt/resource_host_test.go
+++ salt/resource_host_test.go
@@ -1,4 +1,4 @@
-package ansible
+package salt
 
 import (
 	"fmt"
@@ -8,12 +8,12 @@ import (
 	"github.com/hashicorp/terraform/terraform"
 )
 
-func TestAnsibleHost(t *testing.T) {
+func TestSaltHost(t *testing.T) {
 	r.Test(t, r.TestCase{
 		Providers: testAccProviders,
 		Steps: []r.TestStep{
 			r.TestStep{
-				Config: testAnsibleHostConfig,
+				Config: testSaltHostConfig,
 				Check: func(s *terraform.State) error {
 					rID := s.RootModule().Outputs["host_id"].Value
 					if "example.medstack.net" != rID {
@@ -27,17 +27,12 @@ func TestAnsibleHost(t *testing.T) {
 	})
 }
 
-var testAnsibleHostConfig = `
-resource "ansible_host" "test" {
-	inventory_hostname = "example.medstack.net"
-	groups = ["dbservers"]
-	vars = {
-		"first" = "foo"
-		"second" = "bar"
-	}
+var testSaltHostConfig = `
+resource "salt_host" "test" {
+	host = "example.medstack.net"
 }
 
 output "host_id" {
-	value = "${ansible_host.test.id}"
+	value = "${salt_host.test.id}"
 }
 `
