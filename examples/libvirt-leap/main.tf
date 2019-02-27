0c206adef9d87cc60dc104d306690b91d6465e2b
Duncan Mac-Vicar P <dmacvicar@suse.de>
2019-02-27 12:35:55 +0100

    Format examples

--- examples/libvirt-leap/main.tf
+++ examples/libvirt-leap/main.tf
@@ -4,6 +4,7 @@ provider "libvirt" {
 
 resource "libvirt_cloudinit" "common_init" {
   name = "test-init.iso"
+
   user_data = <<EOF
 #cloud-config
 disable_root: 0
@@ -16,49 +17,53 @@ EOF
 }
 
 resource "libvirt_volume" "opensuse_leap" {
-  name = "leap.iso"
+  name   = "leap.iso"
   source = "openSUSE-Leap-42.3-JeOS-for-OpenStack-Cloud.x86_64.qcow2"
 }
 
 resource "libvirt_volume" "volume-web" {
-  name = "volume-web-${count.index}"
+  name           = "volume-web-${count.index}"
   base_volume_id = "${libvirt_volume.opensuse_leap.id}"
-  count = 2
+  count          = 2
 }
 
 resource "libvirt_volume" "volume-db" {
-  name = "volume-db-${count.index}"
+  name           = "volume-db-${count.index}"
   base_volume_id = "${libvirt_volume.opensuse_leap.id}"
-  count = 2
+  count          = 2
 }
 
 resource "libvirt_domain" "vm-web" {
-  name = "web-${count.index}"
+  name   = "web-${count.index}"
   memory = 512
+
   disk {
     volume_id = "${element(libvirt_volume.volume-web.*.id, count.index)}"
   }
 
   network_interface {
-    network_name = "default"
+    network_name   = "default"
     wait_for_lease = true
   }
+
   cloudinit = "${libvirt_cloudinit.common_init.id}"
 
   count = 2
 }
 
 resource "libvirt_domain" "vm-db" {
-  name = "db-${count.index}"
+  name   = "db-${count.index}"
   memory = 512
+
   disk {
     volume_id = "${element(libvirt_volume.volume-db.*.id, count.index)}"
   }
 
   network_interface {
-    network_name = "default"
+    network_name   = "default"
     wait_for_lease = true
   }
+
   cloudinit = "${libvirt_cloudinit.common_init.id}"
 
   count = 2
@@ -66,18 +71,16 @@ resource "libvirt_domain" "vm-db" {
 
 resource "salt_host" "webminion" {
   salt_id = "web${count.index}"
-  host = "${element(libvirt_domain.vm-web.*.network_interface.0.addresses[count.index], 0)}"
-  user = "root"
-  count = 2
-  passwd = ""
+  host    = "${element(libvirt_domain.vm-web.*.network_interface.0.addresses[count.index], 0)}"
+  user    = "root"
+  count   = 2
+  passwd  = ""
 }
 
 resource "salt_host" "dbminion" {
   salt_id = "db${count.index}"
-  host = "${element(libvirt_domain.vm-db.*.network_interface.0.addresses[count.index], 0)}"
-  user = "root"
-  count = 2
-  passwd = ""
+  host    = "${element(libvirt_domain.vm-db.*.network_interface.0.addresses[count.index], 0)}"
+  user    = "root"
+  count   = 2
+  passwd  = ""
 }
-
-
