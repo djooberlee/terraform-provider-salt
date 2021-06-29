c1fe25d39196fe7ed3bb4e7124640c3f4cddc7aa
Duncan Mac-Vicar P <duncan@mac-vicar.eu>
2021-06-29 18:06:20 +0200

    Move to terraform sdk

--- main.go
+++ main.go
@@ -2,11 +2,26 @@ package main
 
 import (
 	"github.com/dmacvicar/terraform-provider-salt/salt"
-	"github.com/hashicorp/terraform/plugin"
+	"github.com/hashicorp/terraform-plugin-sdk/plugin"
 )
 
+var version = "was not built correctly" // set via the Makefile
+
 func main() {
+	versionFlag := flag.Bool("version", false, "print version information and exit")
+	flag.Parse()
+	if *versionFlag {
+		printVersion(os.Stdout)
+		os.Exit(0)
+	}
+
+	defer libvirt.CleanupLibvirtConnections()
+
 	plugin.Serve(&plugin.ServeOpts{
-		ProviderFunc: salt.Provider,
+		ProviderFunc: libvirt.Provider,
 	})
+}
+
+func printVersion(writer io.Writer) {
+	fmt.Fprintf(writer, "%s %s\n", os.Args[0], version)
 }
